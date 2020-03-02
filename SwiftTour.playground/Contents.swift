6bd49e926373d630cad266b95cef6401f6e700c4
felixliang <lziceman08@163.com>
2020-03-02 09:08:08 +0800

    new code add

--- SwiftTour.playground/Contents.swift
+++ SwiftTour.playground/Contents.swift
@@ -760,5 +760,72 @@ let cityAnimals: Set = ["🐦", "🐭"]
 houseAnimals.isSubset(of: farmAnimals)
 farmAnimals.isSuperset(of: houseAnimals)
 houseAnimals.isDisjoint(with: cityAnimals)
-//---------------------------------------
+//------------------switch---------------------
+let anotherCharacter: Character = "a"
+//switch anotherCharacter {
+//case "a":
+//case "A":
+//    print("A")
+//default:
+//    print("a")
+//}
 
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+let naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings)")
+
+let somePoint = (1,1)
+switch somePoint {
+case (0,0):
+    print("\(somePoint) is at origin")
+case (_,0):
+    print("\(somePoint) is at x-axis")
+case (0,_):
+    print("\(somePoint) is at y-axis")
+case (-2...2,-2...2):
+    print("\(somePoint) is inside the box")
+default:
+    print("\(somePoint) is out of the box")
+}
+
+let anotherPoint = (2,0)
+switch anotherPoint {
+case (let x,0):
+    print("on the x-axis with an x value of \(x)")
+case (0,let y):
+    print("on the y-axis with an y value of \(y)")
+case (let x, let y):
+    print("somewhere else at (\(x),\(y))")
+}
+
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x,y) where x == y:
+    print("(\(x),\(y)) is on the line x==y")
+case let (x,y) where x == -y:
+    print("(\(x),\(y)) is on the line x==-y")
+case let (x,y):
+    print("(\(x),\(y)) is just some arbitrary point.")
+}
+
+
+if #available(iOS 10,macOS 10.12, *) {
+    print("1")
+}else {
+    print("2")
+}
